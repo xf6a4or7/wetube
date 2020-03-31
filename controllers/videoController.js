6892d9f2a92145468866fe655a1bd1bb6254e17c
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-01 03:18:34 +0900

    #2.17 Template Variables in Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,11 @@
-export const home = (req, res) => res.render("home");
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", { pageTitle: "home" });
+export const search = (req, res) =>
+  res.render("search", { pageTitle: "search" });
+export const upload = (req, res) =>
+  res.render("upload", { pageTitle: "upload" });
+export const videoDetail = (req, res) =>
+  res.render("videoDetail", { pageTitle: "video detail" });
+export const editVideo = (req, res) =>
+  res.render("editVideo", { pageTitle: "edit video" });
+export const deleteVideo = (req, res) =>
+  res.render("deleteVideo", { pageTitle: "delete video" });
