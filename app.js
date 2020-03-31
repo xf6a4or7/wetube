984353ebd2539c18a8a5a3343e9147908fd09ae6
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-01 01:33:59 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
@@ -13,25 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
-const betweenHome = (req, res, next) => {
-  console.log("Between");
-  next();
-};
-
-app.use(betweenHome);
-
-const handleHome = (req, res) => {
-  res.send("Hello from home");
-};
-
-const handleProfile = (req, res) => {
-  res.send("You are on my profile");
-};
-
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
