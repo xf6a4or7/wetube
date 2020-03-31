b033894002508d6e29af4761f999176982f24787
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-01 02:17:38 +0900

    #2.13 Installing Pug

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -10,7 +10,6 @@ import {
 
 const userRouter = express.Router();
 
-userRouter.get(routes.users, users);
 userRouter.get(routes.editProfile, editProfile);
 userRouter.get(routes.changePassword, changePassword);
 userRouter.get(routes.userDetail, userDetail);
