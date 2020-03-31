fa436a530a4401327be2334c489697b0a3f4a544
정우진 <jeong-ujin@jeong-ujin-ui-MacBook-Pro.local>
2020-04-01 01:06:48 +0900

    #2.7 Express Core Middlewares part Two

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
 const PORT = 4000;
 
-const handleListening = (req, res) => {
-  console.log(`Listening on http://localhost:${PORT}`);
-};
-
-app.listen(PORT, handleListening);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 
 const betweenHome = (req, res, next) => {
   console.log("Between");
